407e4b36795776280985868cb2db4b6d449d49bd
razdechantal <raz.dechantal@gmail.com>
2023-10-13 17:23:20 +0200

    Some changes added

--- rockPaperScisor.cpp
+++ rockPaperScisor.cpp
@@ -15,11 +15,11 @@ int solution(string &G)
         {
             if (gesture == G[i])
             {
-                score += 1; // Tie
+                score += 1; // One score for Tie
             }
             else if ((gesture == 'R' && G[i] == 'S') || (gesture == 'S' && G[i] == 'P') || (gesture == 'P' && G[i] == 'R'))
             {
-                score += 2; // Win
+                score += 2; // Two scores for Win
             }
         }
         max_score = max(max_score, score);
@@ -34,16 +34,19 @@ int main()
     string example2 = "SRR";
     string example3 = "PRPRRPP";
     string example4 = "PPPPRRSSSSS";
+    string example5 = "PSR";
 
     int result1 = solution(example1);
     int result2 = solution(example2);
     int result3 = solution(example3);
     int result4 = solution(example4);
+    int result5 = solution(example5);
 
     cout << "Example 1: " << result1 << " points\n";
     cout << "Example 2: " << result2 << " points\n";
     cout << "Example 3: " << result3 << " points\n";
     cout << "Example 4: " << result4 << " points\n";
+    cout << "Example 5: " << result5 << " points\n";
 
     return 0;
 }
--- timeInSecGame.cpp
+++ timeInSecGame.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 string solution(int T)
 {
+    if (T < 0)
+    {
+        return "Wrong input";
+    }
+
     int hours = T / 3600;
     int minutes = (T % 3600) / 60;
     int seconds = T % 60;
@@ -17,12 +22,17 @@ int main()
 {
     int example1 = 7500;
     int example2 = 83643;
+    int example3 = 33333;
 
     string result1 = solution(example1);
     string result2 = solution(example2);
+    string result3 = solution(example3);
+    string result4 = solution(-1);
 
     cout << "Example 1: " << result1 << endl;
     cout << "Example 2: " << result2 << endl;
+    cout << "Example 3: " << result3 << endl;
+    cout << "Example 4: " << result4 << endl;
 
     return 0;
 }
